79130da065f6be6fb537f2b8a44102acca731909
bakastev <127332568+bakastev@users.noreply.github.com>
2024-01-10 14:25:04 +0100

    Update index.ts

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -1,9 +1,8 @@
-// index.ts
 import { types } from 'react-bricks/frontend'
 
 import HeroUnit from './custom/MyHeroUnit'
 import Pokemon from './custom/Pokemon'
-import MagicCard from './MagicCard' // Importieren Sie Ihre neue Komponente
+import MagicCard from './MagicCard' 
 import reactBricksUITheme from './react-bricks-ui'
 
 const bricks: types.Theme[] = [
@@ -13,18 +12,15 @@ const bricks: types.Theme[] = [
     categories: [
       {
         categoryName: 'Hero sections',
-        bricks: [HeroUnit], // Custom Bricks
+        bricks: [HeroUnit, MagicCard], // Custom Bricks
       },
       {
         categoryName: 'Pokemon',
         bricks: [Pokemon], // External data Bricks
-      },
-      {
-        categoryName: 'Custom', // Erstellen Sie eine neue Kategorie oder fügen Sie sie einer bestehenden hinzu
-        bricks: [MagicCard], // Fügen Sie hier Ihre neue Komponente hinzu
       },
     ],
   },
 ]
 
 export default bricks
+
